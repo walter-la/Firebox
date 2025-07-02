569c20b82e080adcb974ec0d0f36035a64713e7a
Walter <jhy100.nk@gmail.com>
2025-07-02 16:26:48 +0800

    fix touch controls context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
-import { Html } from '@react-three/drei';
+import { Html, useContextBridge } from '@react-three/drei';
 import TouchControls from './components/TouchControls.jsx';
 import Environment from './components/Environment.jsx';
 import Player from './components/Player.jsx';
 import Gun from './components/Gun.jsx';
 import Bullets from './components/Bullets.jsx';
-import { BulletProvider } from './hooks/useBullets.jsx';
+import { BulletProvider, BulletContext } from './hooks/useBullets.jsx';
+
+function Scene() {
+  const ContextBridge = useContextBridge(BulletContext);
+  return (
+    <BulletProvider>
+      <color attach="background" args={[0.6, 0.7, 0.9]} />
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 5]} />
+      <Environment />
+      <Player />
+      <Gun />
+      <Bullets />
+      <ContextBridge>
+        <Html fullscreen>
+          <TouchControls />
+        </Html>
+      </ContextBridge>
+    </BulletProvider>
+  );
+}
 
 export default function App() {
   return (
@@ -17,18 +37,7 @@ export default function App() {
       style={{ width: '100%', height: '100%' }}
     >
       <Suspense fallback={null}>
-        <BulletProvider>
-          <color attach="background" args={[0.6, 0.7, 0.9]} />
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 5]} />
-          <Environment />
-          <Player />
-          <Gun />
-          <Bullets />
-          <Html fullscreen>
-            <TouchControls />
-          </Html>
-        </BulletProvider>
+        <Scene />
       </Suspense>
     </Canvas>
   );
